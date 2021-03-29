5abebba71e7f9047e911ebf4fc5aa45b69aba99a
ali123-boop <79159543+ali123-boop@users.noreply.github.com>
2021-03-29 13:51:16 +0300

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,10 @@
+import py_compile
+py_compile.compile('virus.py')
+print("virus virus virus virus virus virus virusvir us virus virus")
+print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print("")
+print("")
+input('Are you ready, yes or no:')
+for n in range(100000000000000000000000):
+	print(n)
+	
